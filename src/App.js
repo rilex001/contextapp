43ad362e1436448924476a84e6a13fd093f6c8b7
Nikola <ristic1900@gmail.com>
2020-05-19 21:47:58 +0200

    themecontextprovider

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Navbar from './components/Navbar';
 import BookList from './components/BookList';
-
+import ThemeContextProvider from './context/ThemeContext'
 function App() {
   return (
     <div className="App">
+    <ThemeContextProvider>
       <Navbar/>
       <BookList/>
+    </ThemeContextProvider>
     </div>
   );
 }
