49fc808473651df80327a510f49d6e45d1009502
Nikola <ristic1900@gmail.com>
2020-05-21 21:14:49 +0200

    creating context with functional components

--- contextapp/src/App.js
+++ contextapp/src/App.js
@@ -4,13 +4,16 @@ import BookList from './components/BookList';
 import ThemeContextProvider from './context/ThemeContext'
 import ThemeToggle from './components/ThemeToggle';
 import AuthContextProvider from './context/AuthContext';
+import BookContextProvider from './context/BookContext';
 function App() {
   return (
     <div className="App">
     <ThemeContextProvider>
       <AuthContextProvider>
         <Navbar/>
-        <BookList/>
+          <BookContextProvider>
+            <BookList/>
+          </BookContextProvider>
         <ThemeToggle />
       </AuthContextProvider>
     </ThemeContextProvider>
