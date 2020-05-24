0df14621d250476b5c2fd35d82e5f574b7e86eff
Nikola <ristic1900@gmail.com>
2020-05-24 23:34:11 +0200

    style

--- booklist/src/components/Navbar.js
+++ booklist/src/components/Navbar.js
@@ -5,7 +5,7 @@ const Navbar = () => {
     const {books} = useContext(BookContext)
     return (
         <div className='navbar'>
-            <h1>Ninja reading List</h1>
+            <h1>Reading List</h1>
             <p>Currently you have { books.length } books to get through...</p>
         </div>
     )
